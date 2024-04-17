ba3413d8b1598b326c4e290d52135468858e3c34
amitgautam31 <amitgautam31@gmail.com>
2024-04-17 17:59:15 +0530

    discriminated unions are not working 😭

--- src/app/landing-page/page.tsx
+++ src/app/landing-page/page.tsx
@@ -11,7 +11,9 @@ export const runtime = "edge";
 async function getClientDetails(token: string) {
   try {
     const res = await fetch(BASE_URL + "/api/client-profile?token=" + token);
-    const data = await res.json();
+    const data = (await res.json()) as
+      | { error: any }
+      | { result: { Id: string } };
     console.log("data", data);
     // @ts-expect-error
     if (data?.error !== undefined) {
@@ -49,9 +51,8 @@ async function Landing({
         </div>
       </section>
       <section className="flex-1 font-medium text-4xl min-h-[100dvh] flex flex-col items-center justify-between">
-        <h1 className="text-xl text-center">
-          UserData : {JSON.stringify(data)}
-        </h1>
+        {/* @ts-expect-error */}
+        <h1 className="text-xl text-center">client-id :{data?.result?.Id}</h1>
         <div className="px-8 flex flex-col items-center">
           <p className="text-center text-7xl font-medium text-balance py-8 transition-all duration-700 ease-in-out">
             We build brands and digital flagship stores
