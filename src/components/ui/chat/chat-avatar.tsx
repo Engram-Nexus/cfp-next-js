8d0ee660c0962fb4da016746d6cf7c664e73a14d
amitgautam31 <amitgautam31@gmail.com>
2024-04-26 19:11:43 +0530

    user image

--- src/components/ui/chat/chat-avatar.tsx
+++ src/components/ui/chat/chat-avatar.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useVisitorStore } from "@/store/useVisitorStore";
 import { User2 } from "lucide-react";
-import Image from "next/image";
 
 export default function ChatAvatar({ role }: { role: string }) {
   const visitorData = useVisitorStore((state) => state.visitorData);
@@ -10,15 +9,13 @@ export default function ChatAvatar({ role }: { role: string }) {
       <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-background shadow">
         {/* <User2 className="h-4 w-4" /> */}
         {visitorData?.profileImage ? (
-          <object data={visitorData?.profileImage} type="image/png">
-            <img
-              className="rounded-md"
-              src="user.png"
-              alt="visitor Image"
-              width={24}
-              height={24}
-            />
-          </object>
+          <img
+            className="rounded-md"
+            src={visitorData?.profileImage}
+            alt="visitor Image"
+            width={24}
+            height={24}
+          />
         ) : (
           <User2 className="h-4 w-4" />
         )}
@@ -27,20 +24,15 @@ export default function ChatAvatar({ role }: { role: string }) {
   }
   return (
     <div className="flex h-8 w-8 shrink-0 select-none items-center justify-center rounded-md border bg-black text-white shadow">
-      <object
-        data={
+      <img
+        className="rounded-md"
+        src={
           visitorData?.companyLogo ? visitorData?.companyLogo : "/engram.png"
         }
-        type="image/png"
-      >
-        <img
-          className="rounded-md"
-          src="/engram.png"
-          alt="engram Logo"
-          width={24}
-          height={24}
-        />
-      </object>
+        alt="engram Logo"
+        width={24}
+        height={24}
+      />
     </div>
   );
 }
