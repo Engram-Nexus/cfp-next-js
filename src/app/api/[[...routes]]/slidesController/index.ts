fc7bbc9172c3b3863d3f82c5eecb4bfcc6351809
amitgautam31 <amitgautam31@gmail.com>
2024-05-04 18:19:32 +0530

    slides controller api

--- src/app/api/[[...routes]]/slidesController/index.ts
+++ src/app/api/[[...routes]]/slidesController/index.ts
@@ -1,3 +1,4 @@
+import { decrypt, encrypt } from "@/lib/jwt";
 import { Hono } from "hono";
 import OpenAI from "openai";
 
@@ -13,16 +14,48 @@ slidesControllerApi.post("/", async (c) => {
       assistantId?: string;
       filePath?: string;
       prompt?: string;
+      slideUrl?: string;
     }>();
 
     let file = body["file"];
     let assistantId = body["assistantId"];
+    let slideUrl = body["slideUrl"];
     const filePath = body["filePath"];
 
-    if (!filePath && !file) {
+    if (!filePath && !file && !slideUrl) {
       return c.json({ error: "File Path is required" }, { status: 400 });
     }
-    
+    if (slideUrl) {
+      const match = slideUrl.match(/\/d\/(?:e\/)?([^/]+)/);
+      if (match) {
+        try {
+          const presentationId = match[1];
+          const reposnse = await fetch("http://localhost:3000/api/slides", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              presentationId,
+            }),
+          });
+          let data = (await reposnse.json() as { data: any }).data;
+          data={...data,slides:data.slides.map((s:string[],idx:number)=>({...s,slideNumber:idx+1}))}
+          file = new File([JSON.stringify(data)], "file.json", {
+            type: "application/json",
+          });
+        } catch (err) {
+          console.log(err);
+        }
+        //  .then((res) => res.json() ).catch((err) => console.log(err))
+      } else {
+        return Response.json(
+          { error: "Unable to extract presentation ID from the slide URL" },
+          { status: 400 }
+        );
+      }
+    }
+
     if (filePath && !file) {
       // here we are creating a file from the filePath
       const response = await fetch(filePath);
@@ -49,7 +82,7 @@ slidesControllerApi.post("/", async (c) => {
         await openai.beta.assistants.create({
           model: "gpt-3.5-turbo",
           name: "slides controller new",
-          instructions: "read the data from the slides",
+          instructions: "read the data from the slides and also send the source of the data to the user as a slide number in json format, slide number is mandatory and it start from 0 so add 1 to the slide number",
           tool_resources: {
             file_search: {
               vector_store_ids: [vectorStore.id],
@@ -68,11 +101,21 @@ slidesControllerApi.post("/", async (c) => {
         tool_resources: { file_search: { vector_store_ids: [vectorStore.id] } },
       });
     }
-    return c.json({ assistantId });
+    const token = await encrypt({ assistantId, slideUrl });
+    const url = "http://localhost:3001" + "/slides-controller?token=" + token;
+    return c.json({ url }, { status: 200 });
   } catch (error) {
     console.error(error);
-    return c.json({ error}, { status: 500 });
+    return c.json({ error }, { status: 500 });
   }
+});
+slidesControllerApi.get("/", async (c) => {
+  const { token } = c.req.query();
+  if (!token) {
+    return c.json({ error: "Token is required" }, { status: 400 });
+  }
+  const decodeData = await decrypt(token);
+  return c.json({ decodeData }, { status: 200 });
 });
 
 export default slidesControllerApi;
--- src/app/slides-controller/page.tsx
+++ src/app/slides-controller/page.tsx
@@ -1,17 +1,15 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { cn } from "@/lib/utils";
-import {
-  SquareChevronLeft,
-  SquareChevronRight,
-} from "lucide-react";
+import { SquareChevronLeft, SquareChevronRight } from "lucide-react";
 import { experimental_useAssistant as useAssistant } from "ai/react";
 import { ChatInput, ChatMessages } from "@/components/ui/chat";
 import { Button } from "@/components/ui/button";
-const GoogleSlidesChat = () => {
+import { decrypt } from "@/lib/jwt";
+const GoogleSlidesChat = ({searchParams:{token}}:{searchParams:{token:string}}) => {
   const [open, setOpen] = useState(true);
   const chatSectionRef = useRef<HTMLDivElement>(null);
-
+const [data, setData] = useState<any>(null)
   const {
     messages,
     input,
@@ -27,12 +25,20 @@ const GoogleSlidesChat = () => {
     setOpen(!open);
   };
   // const getSlides = ()=>{
+  // }a
 
-  // }
-  const onsubmit = () => {
-    
-  }
-  console.log("messages", messages);
+const  decode =async()=>{
+  const url = token
+  const decodedData = await decrypt(url);;
+return decodedData
+}
+ 
+useEffect(() => {
+  decode().then((decodedData)=>{
+setData(decodedData)
+  })
+},[])
+
 
   return (
     <div className="md:flex h-[100vh] w-full">
@@ -46,7 +52,8 @@ const GoogleSlidesChat = () => {
           id="googleSlideIframe"
           className="w-full h-full"
           // src="https://docs.google.com/presentation/d/e/2PACX-1vSOwXWZZMSVJ3Lk03_mz7pFlpMDuf1FRBxPAbUsvS_hVvzmlk-uz2vI78avvBairfM2vdBLbrEnr5yX/embed?start=false&loop=false&delayms=3000"
-          src="  https://docs.google.com/presentation/d/1CrUeeIpwJCqlmicm8T3UpQG9XAMZAX9bs3-VeX2F8sg/embed?start=false&loop=false&delayms=3000"
+          src={data?.slideUrl+"/embed?start=false&loop=false&delayms=3000" }
+    
           allowFullScreen={true}
         ></iframe>
         <button
@@ -72,21 +79,7 @@ const GoogleSlidesChat = () => {
           <p className="text-center bg-[#0f172a] p-2 rounded-md text-shadow:2px 2px 20px #085FA0 text-[#FFFFFF] ">
             Ask questions about your slides !
           </p>
-          {/* {chats.map((msg: any) => (
-            <div key={msg.message} className="mt-6">
-              <div className="flex justify-end">
-                <p className="break-all bg-[#007bff] font-normal p-2 my-2 shadow-sm rounded-tl-xl rounded-br-xl w-fit text-white">
-                  {msg.question}
-                </p>
-              </div>
-              {msg.answer && (
-                <p className="break-all font-normal rounded-tr-xl rounded-bl-xl bg-white p-2 my-2 shadow-sm  w-fit text-black">
-                  {msg.answer}
-                </p>
-              )}
-            </div>
-          ))} */}
-    
+
           <div className="flex flex-col justify-between gap-6">
             <ChatMessages
               from="slides"
@@ -101,39 +94,18 @@ const GoogleSlidesChat = () => {
                 console.log("stop");
               }}
               // flex1={flex1}
-              />
+            />
             <ChatInput
               input={input}
               handleSubmit={handleSubmit}
               handleInputChange={handleInputChange}
               isLoading={status === "in_progress"}
-              threadId={threadId?threadId:""}
-              assistantId={"asst_ud8uuERilaRiXv5W9XD3LHLJ"}
+              threadId={threadId ? threadId : ""}
+              // assistantId={"asst_ud8uuERilaRiXv5W9XD3LHLJ"}
+              assistantId={data?.assistantId}
             />
           </div>
         </div>
-        {/* {isLoading && <LoaderCircle className="animate-spin" />}
-        <form onSubmit={handleSubmit} className="flex gap-2 items-center">
-          <input
-            value={input}
-            id="chatInput"
-            onChange={handleInputChange}
-            className="w-full border-2 focus:outline-none border-slate-200 p-2 rounded-md placeholder:text-black"
-            type="text"
-            placeholder="Enter your message..."
-            onKeyUp={(event) => {
-              if (event.key === "Enter") handleSubmit();
-            }}
-          />
-          <Button type="submit" className="text-md flex items-center">
-            Send
-            <span className="ml-2 text-2xl">&#8679;</span>
-          </Button>
-        </form> */}
-                  {/* <Button type="submit" className="text-md flex items-center">
-            Send
-            <span className="ml-2 text-2xl">&#8679;</span>
-          </Button> */}
       </div>
     </div>
   );
