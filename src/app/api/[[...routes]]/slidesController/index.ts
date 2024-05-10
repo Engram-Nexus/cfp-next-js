0a01d1aacc1e911809cc621395bde5bbafbc11c8
amitgautam31 <amitgautam31@gmail.com>
2024-05-10 17:44:38 +0530

    added instruction

--- src/app/api/[[...routes]]/slidesController/index.ts
+++ src/app/api/[[...routes]]/slidesController/index.ts
@@ -10,13 +10,13 @@ slidesControllerApi.post("/", async (c) => {
     const openai = new OpenAI({
       apiKey: process.env.OPENAI_API_KEY,
     });
-    let { slidesData, assistantId ,  slideUrl } = await c.req.json<{
+    let { slidesData, assistantId, slideUrl } = await c.req.json<{
       slidesData: string[];
-      assistantId?: string ;
+      assistantId?: string;
       slideUrl: string;
     }>();
 
-    if (!slidesData&&!slideUrl) {
+    if (!slidesData && !slideUrl) {
       return c.json({ error: "Slides are required" }, { status: 400 });
     }
 
@@ -24,11 +24,9 @@ slidesControllerApi.post("/", async (c) => {
       type: "application/json",
     });
 
-
     const vectorStore = await openai.beta.vectorStores.create({
       name: "slides controller",
     });
-
 
     const res = await openai.beta.vectorStores.fileBatches.uploadAndPoll(
       vectorStore.id,
@@ -42,8 +40,8 @@ slidesControllerApi.post("/", async (c) => {
         await openai.beta.assistants.create({
           model: "gpt-3.5-turbo",
           name: "google slides assistant",
-            instructions:
-            "read the data from slides document , and also send the slide number as a source of information",
+          instructions:
+            "read the data from slides document , and also send the slide number as a source of information , if no information is found then respond with 'no information found , please ask the question regarding to this presentation'",
           tool_resources: {
             file_search: {
               vector_store_ids: [vectorStore.id],
@@ -63,7 +61,7 @@ slidesControllerApi.post("/", async (c) => {
       });
     }
     const token = await encrypt({ assistantId, slideUrl });
-    const url = BASE_URL+ "/slides?token=" + token;
+    const url = BASE_URL + "/slides?token=" + token;
     return c.json({ url }, { status: 200 });
   } catch (error) {
     console.log(error);
